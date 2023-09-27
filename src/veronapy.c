54d9cbd308cdbf674bb0c5d63ba135d185ee61ba
Matthew A Johnson <matjoh@microsoft.com>
2023-09-27 23:57:01 +0100

    Basic object capture working
    Signed-off-by: Matthew A Johnson <matjoh@microsoft.com>

--- src/veronapy.c
+++ src/veronapy.c
@@ -10,28 +10,60 @@ typedef long bool;
 #define true 1
 #define false 0
 
-typedef struct {
-  PyObject_HEAD PyObject *name;
+static const char *REGION_ATTRS[] = {
+    "name",
+    "__identity__",
+    "__region__",
+    "is_open",
+    "is_shared",
+    "__enter__",
+    "__exit__",
+    NULL,
+};
+
+static PyModuleDef veronapymoduledef = {
+    PyModuleDef_HEAD_INIT,
+    .m_name = "veronapy",
+    .m_doc = "veronapy is a Python extension that adds Behavior-oriented "
+             "Concurrency runtime for Python.",
+    .m_size = -1,
+};
+
+static PyObject *veronapymodule = NULL;
+
+////// Region //////
+
+typedef struct
+{
+  PyObject_HEAD;
+  PyObject *name;
   PyObject *alias;
   unsigned long long __identity__;
   bool is_open;
   bool is_shared;
   PyObject *__region__;
+  PyObject *objects;
 } RegionObject;
 
-static void Region_dealloc(RegionObject *self) {
+static void Region_dealloc(RegionObject *self)
+{
   Py_XDECREF(self->name);
   Py_XDECREF(self->alias);
+  Py_XDECREF(self->__region__);
+  Py_DECREF(self->objects);
   Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
 static PyObject *Region_new(PyTypeObject *type, PyObject *args,
-                            PyObject *kwds) {
+                            PyObject *kwds)
+{
   RegionObject *self;
   self = (RegionObject *)type->tp_alloc(type, 0);
-  if (self != NULL) {
+  if (self != NULL)
+  {
     self->name = PyUnicode_FromString("");
-    if (self->name == NULL) {
+    if (self->name == NULL)
+    {
       Py_DECREF(self);
       return NULL;
     }
@@ -42,12 +74,19 @@ static PyObject *Region_new(PyTypeObject *type, PyObject *args,
     self->is_open = false;
     self->is_shared = false;
     self->__identity__ = 0;
+    self->objects = PyDict_New();
+    if (self->objects == NULL)
+    {
+      Py_DECREF(self);
+      return NULL;
+    }
   }
 
   return (PyObject *)self;
 }
 
-static int Region_init(RegionObject *self, PyObject *args, PyObject *kwds) {
+static int Region_init(RegionObject *self, PyObject *args, PyObject *kwds)
+{
   static char *kwlist[] = {"name", NULL};
   PyObject *name = NULL, *tmp;
 
@@ -56,35 +95,43 @@ static int Region_init(RegionObject *self, PyObject *args, PyObject *kwds) {
 
   self->__identity__ = atomic_fetch_add(&region_identity, 1);
 
-  if (name) {
+  if (name)
+  {
     tmp = self->name;
     Py_INCREF(name);
     self->name = name;
     Py_XDECREF(tmp);
-  } else {
+  }
+  else
+  {
     self->name = PyUnicode_FromFormat("region_%llu", self->__identity__);
-    if (self->name == NULL) {
+    if (self->name == NULL)
+    {
       Py_DECREF(self);
-      return NULL;
+      return -1;
     }
   }
 
   return 0;
 }
 
-static PyObject *Region_getname(RegionObject *self, void *closure) {
+static PyObject *Region_getname(RegionObject *self, void *closure)
+{
   Py_INCREF(self->name);
   return self->name;
 }
 
-static int Region_setname(RegionObject *self, PyObject *value, void *closure) {
+static int Region_setname(RegionObject *self, PyObject *value, void *closure)
+{
   PyObject *tmp;
-  if (value == NULL) {
+  if (value == NULL)
+  {
     PyErr_SetString(PyExc_TypeError, "Cannot delete the name attribute");
     return -1;
   }
 
-  if (!PyUnicode_Check(value)) {
+  if (!PyUnicode_Check(value))
+  {
     PyErr_SetString(PyExc_TypeError,
                     "The name attribute value must be a string");
     return -1;
@@ -97,42 +144,50 @@ static int Region_setname(RegionObject *self, PyObject *value, void *closure) {
   return 0;
 }
 
-static PyObject *Region_getidentity(RegionObject *self, void *closure) {
+static PyObject *Region_getidentity(RegionObject *self, void *closure)
+{
   return PyLong_FromUnsignedLongLong(self->__identity__);
 }
 
 static int Region_setidentity(RegionObject *self, PyObject *value,
-                              void *closure) {
+                              void *closure)
+{
   PyErr_SetString(PyExc_TypeError, "Cannot set the __identity__ attribute");
   return -1;
 }
 
-static PyObject *Region_getregion(RegionObject *self, void *closure) {
+static PyObject *Region_getregion(RegionObject *self, void *closure)
+{
   Py_INCREF(self->__region__);
   return self->__region__;
 }
 
 static int Region_setregion(RegionObject *self, PyObject *value,
-                            void *closure) {
+                            void *closure)
+{
   PyErr_SetString(PyExc_TypeError, "Cannot set the __region__ attribute");
   return -1;
 }
 
-static PyObject *Region_getisopen(RegionObject *self, void *closure) {
+static PyObject *Region_getisopen(RegionObject *self, void *closure)
+{
   return PyBool_FromLong(self->is_open);
 }
 
-static int Rego_setisopen(RegionObject *self, PyObject *value, void *closure) {
+static int Rego_setisopen(RegionObject *self, PyObject *value, void *closure)
+{
   PyErr_SetString(PyExc_TypeError, "Cannot set the is_open attribute");
   return -1;
 }
 
-static PyObject *Region_getisshared(RegionObject *self, void *closure) {
+static PyObject *Region_getisshared(RegionObject *self, void *closure)
+{
   return PyBool_FromLong(self->is_shared);
 }
 
 static int Region_setisshared(RegionObject *self, PyObject *value,
-                              void *closure) {
+                              void *closure)
+{
   PyErr_SetString(PyExc_TypeError, "Cannot set the is_shared attribute");
   return -1;
 }
@@ -151,13 +206,266 @@ static PyGetSetDef Region_getsetters[] = {
     {NULL} /* Sentinel */
 };
 
-static PyObject *Region_str(RegionObject *self, PyObject *Py_UNUSED(ignored)) {
+static PyObject *Region_str(RegionObject *self, PyObject *Py_UNUSED(ignored))
+{
   return PyUnicode_FromFormat("Region(%S<%d>)", self->name, self->__identity__);
 }
 
+static PyObject *Region_enter(RegionObject *self,
+                              PyObject *Py_UNUSED(ignored))
+{
+  self->is_open = true;
+  Py_INCREF((PyObject *)self);
+  return (PyObject *)self;
+}
+
+static PyObject *Region_exit(RegionObject *self, PyObject *args,
+                             PyObject *kwds)
+{
+  PyObject *type, *value, *traceback;
+  if (!PyArg_ParseTuple(args, "OOO", &type, &value, &traceback))
+    return NULL;
+
+  if (type == Py_None)
+  {
+    Py_XDECREF(type);
+    Py_XDECREF(value);
+    Py_XDECREF(traceback);
+    self->is_open = false;
+  }
+  else
+  {
+    PyErr_Restore(type, value, traceback);
+  }
+
+  Py_RETURN_FALSE;
+}
+
 static PyMethodDef Region_methods[] = {
+    {"__enter__", (PyCFunction)Region_enter, METH_NOARGS, "Enter the region"},
+    {"__exit__", (PyCFunction)Region_exit, METH_VARARGS,
+     "Exit the region, returning a Boolean flag indicating whether any "
+     "exception that occurred should be suppressed."},
     {NULL} /* Sentinel */
 };
+
+////// Isolated //////
+
+static void Isolated_finalize(PyObject *self)
+{
+  PyTypeObject *isolated_type = Py_TYPE(self);
+  PyTypeObject *type = (PyTypeObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__isolated__");
+  if (isolated_type == NULL)
+  {
+    printf("error getting __isolated__");
+    return;
+  }
+
+  self->ob_type = type;
+  Py_DECREF(isolated_type);
+}
+
+static PyObject *Isolated_repr(PyObject *self)
+{
+  PyTypeObject *isolated_type = Py_TYPE(self);
+  PyTypeObject *type = (PyTypeObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__isolated__");
+  if (isolated_type == NULL)
+  {
+    printf("error getting __isolated__");
+    return NULL;
+  }
+
+  RegionObject *region = (RegionObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__region__");
+  if (region == NULL)
+  {
+    printf("error getting __region__");
+    return -1;
+  }
+
+  if (region->is_open)
+  {
+    self->ob_type = type;
+    PyObject *repr = PyObject_Repr(self);
+    self->ob_type = isolated_type;
+    return repr;
+  }
+
+  PyErr_SetString(PyExc_RuntimeError, "Region is not open");
+  return NULL;
+}
+
+static int Isolated_traverse(PyObject *self, visitproc visit, void *arg)
+{
+  int rc;
+  PyTypeObject *isolated_type = Py_TYPE(self);
+  PyTypeObject *type = (PyTypeObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__isolated__");
+  if (isolated_type == NULL)
+  {
+    printf("error getting __isolated__");
+    return -1;
+  }
+
+  traverseproc tp_traverse = type->tp_traverse;
+  if (tp_traverse != NULL)
+  {
+    self->ob_type = type;
+    rc = tp_traverse(self, visit, arg);
+    self->ob_type = isolated_type;
+    return rc;
+  }
+
+  return 0;
+}
+
+static int Isolated_clear(PyObject *self)
+{
+  int rc;
+
+  PyTypeObject *isolated_type = Py_TYPE(self);
+  PyTypeObject *type = (PyTypeObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__isolated__");
+  if (isolated_type == NULL)
+  {
+    printf("error getting __isolated__");
+    return -1;
+  }
+
+  inquiry tp_clear = type->tp_clear;
+  if (tp_clear != NULL)
+  {
+    self->ob_type = type;
+    rc = tp_clear(self);
+    self->ob_type = isolated_type;
+    return rc;
+  }
+
+  return 0;
+}
+
+static PyObject *Isolated_getattro(PyObject *self, PyObject *attr_name)
+{
+  PyTypeObject *isolated_type = Py_TYPE(self);
+  PyTypeObject *type = (PyTypeObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__isolated__");
+  if (isolated_type == NULL)
+  {
+    printf("error getting __isolated__");
+    return -1;
+  }
+
+  RegionObject *region = (RegionObject *)PyDict_GetItemString(
+      isolated_type->tp_dict, "__region__");
+  if (region == NULL)
+  {
+    printf("error getting __region__");
+    return -1;
+  }
+
+  if (strcmp(PyUnicode_AsUTF8(attr_name), "__region__") == 0)
+  {
+    Py_INCREF(region);
+    return (PyObject *)region;
+  }
+
+  if (region->is_open)
+  {
+    self->ob_type = type;
+    PyObject *obj = PyObject_GenericGetAttr(self, attr_name);
+    self->ob_type = isolated_type;
+    return obj;
+  }
+
+  PyErr_SetString(PyExc_RuntimeError, "Region is not open");
+  return NULL;
+}
+
+static PyObject *Region_getattro(RegionObject *self, PyObject *attr_name)
+{
+  for (int i = 0; REGION_ATTRS[i] != NULL; i++)
+  {
+    if (strcmp(PyUnicode_AsUTF8(attr_name), REGION_ATTRS[i]) == 0)
+    {
+      return PyObject_GenericGetAttr((PyObject *)self, attr_name);
+    }
+  }
+
+  PyObject *obj = PyDict_GetItemWithError(self->objects, attr_name);
+  if (obj == NULL)
+  {
+    Py_RETURN_NONE;
+  }
+
+  Py_INCREF(obj);
+  return obj;
+}
+
+static int Region_setattro(RegionObject *self, PyObject *attr_name,
+                           PyObject *value)
+{
+  int rc;
+  for (int i = 0; REGION_ATTRS[i] != NULL; i++)
+  {
+    if (strcmp(PyUnicode_AsUTF8(attr_name), REGION_ATTRS[i]) == 0)
+    {
+      return PyObject_GenericSetAttr((PyObject *)self, attr_name, value);
+    }
+  }
+
+  PyTypeObject *type = Py_TYPE(value);
+  if ((type->tp_flags & Py_TPFLAGS_HEAPTYPE) != 0)
+  {
+    PyType_Slot slots[] = {
+        {Py_tp_finalize, Isolated_finalize},
+        {Py_tp_repr, Isolated_repr},
+        {Py_tp_traverse, Isolated_traverse},
+        {Py_tp_clear, Isolated_clear},
+        {Py_tp_getattro, Isolated_getattro},
+        {0, NULL} /* Sentinel */
+    };
+
+    PyType_Spec spec = {
+        .name = "region.isolated",
+        .basicsize = type->tp_basicsize,
+        .itemsize = type->tp_itemsize,
+        .flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HEAPTYPE,
+        .slots = slots,
+    };
+
+    PyTypeObject *isolated_type = (PyTypeObject *)PyType_FromModuleAndSpec(veronapymodule, &spec, NULL);
+    if (isolated_type == NULL)
+    {
+      printf("error creating isolated type");
+      return -1;
+    }
+
+    rc = PyDict_SetItemString(isolated_type->tp_dict, "__isolated__",
+                              (PyObject *)type);
+    if (rc < 0)
+    {
+      printf("error setting __isolated__");
+      return rc;
+    }
+
+    rc = PyDict_SetItemString(isolated_type->tp_dict, "__region__",
+                              (PyObject *)self);
+    if (rc < 0)
+    {
+      printf("error setting __region__");
+      return rc;
+    }
+
+    Py_INCREF((PyObject *)isolated_type);
+    value->ob_type = isolated_type;
+  }
+
+  Py_INCREF(value);
+  rc = PyDict_SetItem(self->objects, attr_name, value);
+  return rc;
+}
 
 static PyTypeObject RegionType = {
     PyVarObject_HEAD_INIT(NULL, 0).tp_name = "veronapy.region",
@@ -171,33 +479,28 @@ static PyTypeObject RegionType = {
     .tp_methods = Region_methods,
     .tp_getset = Region_getsetters,
     .tp_str = (reprfunc)Region_str,
+    .tp_setattro = (setattrofunc)Region_setattro,
+    .tp_getattro = (getattrofunc)Region_getattro,
 };
 
-static PyModuleDef veronapymodule = {
-    PyModuleDef_HEAD_INIT,
-    .m_name = "veronapy",
-    .m_doc = "veronapy is a Python extension that adds Behavior-oriented "
-             "Concurrency runtime for Python.",
-    .m_size = -1,
-};
-
-PyMODINIT_FUNC PyInit_veronapy(void) {
-  PyObject *m;
+PyMODINIT_FUNC PyInit_veronapy(void)
+{
   if (PyType_Ready(&RegionType) < 0)
     return NULL;
 
-  m = PyModule_Create(&veronapymodule);
-  if (m == NULL)
+  veronapymodule = PyModule_Create(&veronapymoduledef);
+  if (veronapymodule == NULL)
     return NULL;
 
-  PyModule_AddStringConstant(m, "__version__", "0.0.1");
+  PyModule_AddStringConstant(veronapymodule, "__version__", "0.0.1");
 
   Py_INCREF(&RegionType);
-  if (PyModule_AddObject(m, "region", (PyObject *)&RegionType) < 0) {
+  if (PyModule_AddObject(veronapymodule, "region", (PyObject *)&RegionType) < 0)
+  {
     Py_DECREF(&RegionType);
-    Py_DECREF(m);
+    Py_DECREF(veronapymodule);
     return NULL;
   }
 
-  return m;
+  return veronapymodule;
 }
--- tests/test_region.py
+++ tests/test_region.py
@@ -1,5 +1,6 @@
 """Region tests."""
 
+import json
 from veronapy import region
 
 
@@ -44,3 +45,59 @@ def test_creation():
 
     assert r1.name == "b"
     assert r0.__identity__ != r1.__identity__
+
+
+def test_open():
+    a = region("a")
+    assert not a.is_open
+    a.__enter__()
+    assert a.is_open
+    a.__exit__(None, None, None)
+    assert not a.is_open
+
+
+class MockObject:
+    """Mock object used for testing."""
+    def __str__(self) -> str:
+        """Produces a simple representation of the object graph."""
+        return json.dumps(self.__dict__)
+
+
+def test_adding():
+    r = region()   # closed, free and private
+    o = MockObject()  # free
+
+    with r:
+        r.f = o       # o becomes owned by r
+
+    assert o.__region__ == r
+
+
+def test_ownership():
+    r1 = region("Bank1")
+    r2 = region("Bank2")
+    with r1, r2:
+        r1.accounts = {"Alice": 1000}
+        try:
+            r2.accounts = r1.accounts
+        except RuntimeError:
+            # ownership exception
+            pass
+        else:
+            raise AssertionError
+
+
+def test_isolation():
+    r1 = region("Bank1")
+    x = None
+    with r1:
+        r1.accounts = {"Alice": 1000}
+        x = r1.accounts
+
+    try:
+        print(x["Alice"])
+    except RuntimeError:
+        # the region not open
+        pass
+    else:
+        raise AssertionError
